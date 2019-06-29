4ae0a1501bd4db500aac90838c08335a1ba2c9cb
Vivek Singh <viveksingh.lp@gmail.com>
2019-06-29 18:27:02 +0530

    Add User model

---
+++ helpers/db.js
@@ -0,0 +1,11 @@
+const config = require('config.json');
+const mongoose = require('mongoose');
+mongoose.connect(config.connectionString, { useCreateIndex: true, useNewUrlParser: true }).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.log(err);
+});
+
+module.exports = {
+  User: require('../models')
+}
